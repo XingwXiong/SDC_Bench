b087c267a2f4119d9ca61c98685f89c28abe78e3
xingwxiong <xingw.xiong@gmail.com>
2018-09-18 16:40:33 +0800

    x86/redis-cluster-bench/redis-openloop-bench.cpp updated

--- x86/redis-cluster-bench/redis-openloop-bench.cpp
+++ x86/redis-cluster-bench/redis-openloop-bench.cpp
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <sys/time.h>
 #include <bits/stdc++.h>
 #include <acl_cpp/lib_acl.hpp>
@@ -13,22 +14,26 @@ struct Config {
     long long display_las_time;
     long long display_las_index;
 
+    enum Status{WARMUP, RUNNING, FINISHED} status; // 0--warmup 1--running 2--finished
+    int sem_cnt;
     string ip_addr;
     int port;
     string auth;
     int requests_finished;
     int requests;
     int qps;
+    int warmup_requests;
 
     vector<string> task; //-t get,set
     string type; // current task type
     long long start_tm;
     long long total_tm;
     long long interval;
+    int round, total_round;
     vector<long long> lats;
 
     pthread_mutex_t lats_mutex;
-
+    FILE* fd_rep, *fd_lat; 
 } config;
 ExpDist dist; // exponential_distribution
 
@@ -103,11 +108,11 @@ bool test_set(acl::redis_string &cmd) {
     acl::string key = gen_key();
     acl::string val = gen_val();
     bool ret = cmd.set(key, val);
-    if (ret == false ) {
+    if (ret == false) {
         const acl::redis_result *res = cmd.get_result();
         printf("set key: error: %s\r\n",
                res ? res->get_error() : "unknown error");
-        //    return false;
+        return false;
     }
     assert(ret == true);
     key.clear();
@@ -119,9 +124,18 @@ bool test_set(acl::redis_string &cmd) {
 bool test_get(acl::redis_string &cmd) {
     acl::string key = gen_key();
     acl::string val = gen_val();
-    assert(cmd.get(key, val) == true);
+    //assert(cmd.get(key, val) == true);
+    bool ret = cmd.get(key, val);
+    if (ret == false) {
+        const acl::redis_result *res = cmd.get_result();
+        printf("get key: error: %s\r\n",
+               res ? res->get_error() : "unknown error");
+        return false;
+    }
+    assert(ret == true);
     key.clear();
     val.clear();
+    cmd.clear();
     return true;
 }
 
@@ -137,35 +151,100 @@ void *processRequests(void *e) {
     }
     acl::redis_string cmd_string;
     cmd_string.set_cluster(&cluster, 100); // max_conn = 100
+    
+    // warmup_request
     pthread_mutex_lock(&config.lats_mutex);
-    while(config.requests_finished < config.requests) {
+    while(config.requests_finished < config.warmup_requests) {
         long long lat_bg = dist.nextArrivalNs();
         if(nstime() < lat_bg) sleepUntilNs(lat_bg);
-        lat_bg = ustime();
         pthread_mutex_unlock(&config.lats_mutex);
         func(cmd_string);
         pthread_mutex_lock(&config.lats_mutex);
         ++ config.requests_finished;
-        config.lats.push_back(ustime() - lat_bg);
+    }
+   // cout << "finished" << endl;
+    -- config.sem_cnt;
+    pthread_mutex_unlock(&config.lats_mutex);
+    while(config.sem_cnt > 0); 
+    pthread_mutex_lock(&config.lats_mutex);
+  //  cout << "begin" << endl;
+    // init
+    config.requests_finished = 0;
+    config.status = Config::RUNNING;
+    // processRequsts
+    while(config.requests_finished < config.requests) {
+        //if(config.round >= config.total_round) break;
+        long long lat_bg = dist.nextArrivalNs();
+        if(nstime() < lat_bg) sleepUntilNs(lat_bg);
+        pthread_mutex_unlock(&config.lats_mutex);
+        lat_bg = ustime();
+        func(cmd_string);
+        long long lat_ed = ustime();
+        pthread_mutex_lock(&config.lats_mutex);
+        ++ config.requests_finished;
+        if(config.round >= config.total_round) continue;
+        config.lats.push_back(lat_ed - lat_bg);
+        config.total_tm = lat_ed - config.start_tm;
     }
     pthread_mutex_unlock(&config.lats_mutex);
+}
+
+void show_total_report() {
+    printf("============================%s============================\n", config.type.c_str());
+    long double mean_lat = 0;
+    for(auto &lat : config.lats) {
+        mean_lat += lat;
+        fprintf(config.fd_lat, "%lld\n", lat);
+    }
+    fflush(config.fd_lat);
+    mean_lat = config.requests_finished ? mean_lat / config.requests_finished : 0;
+    long double qps = config.total_tm ? (long double)config.requests_finished / (config.total_tm / 1000000.0) : 0;
+    sort(config.lats.begin(), config.lats.end());
+    double pers[] = {0, 50, 80, 95, 99, 99.9, 100};
+    int pers_size = 7; //sizeof(pers) / sizeof(double);
+    printf("%8s|%8s", "QPS", "MEAN");
+    for(int i = 0; i < pers_size; ++ i) {
+        printf("|%7.2f%%", pers[i]);
+    }
+    printf(" (unit: us)\t(%10d requests)\n", (int)config.lats.size());
+    printf("%8.2Lf|%8.2Lf", qps, mean_lat);
+    for (int i = 0; i < pers_size; ++ i) {
+        int pos = pers[i] / 100 * max(0, (int)config.lats.size() - 1);
+        printf("|%8lld", config.lats[pos]);
+    }
+    printf("\n");
+    fflush(stdout);
+    printf("============================%s============================\n", "FINISHED");
 }
 
 static void *show_report(void *) {
     static double pers[] = {0, 50, 80, 95, 99, 99.9, 100};
     static int pers_size = 7; //sizeof(pers) / sizeof(double);
-    while(config.display_las_index < config.requests) {
-        int requests_finished = config.requests_finished;
+    while(config.status == Config::WARMUP);
+    pthread_mutex_lock(&config.lats_mutex);
+    config.lats.clear();
+    config.start_tm = config.display_las_time = ustime();
+    config.display_las_index = 0; 
+    pthread_mutex_unlock(&config.lats_mutex);
+    sleepUntilNs(config.interval * 1e9); 
+    
+    while(config.requests_finished < config.requests &&  config.display_las_index < config.requests) {
+        //int requests_finished = config.requests_finished;
+        int requests_finished = config.lats.size();
         long long cur_time = ustime();
         long long delta_tm = cur_time - config.display_las_time;
         long long delta_sz = requests_finished - config.display_las_index;
         if(delta_tm < (long long)config.interval * 1000000) continue;
+        if(config.round >= config.total_round) break;
+        config.round += 1;
         vector<long long> lat_buf;
         long double mean_lat = 0;
+
         for(int i = config.display_las_index; i < requests_finished; ++i) {
             lat_buf.push_back(config.lats[i]);
             mean_lat += config.lats[i];
         }
+        assert(lat_buf.size() != 0);
         mean_lat = delta_sz ? mean_lat / delta_sz : 0;
         long double qps = delta_tm ? (long double) delta_sz / (delta_tm / 1000000.0) : 0;
         sort(lat_buf.begin(), lat_buf.end());
@@ -174,92 +253,69 @@ static void *show_report(void *) {
         for(int i = 0; i < pers_size; ++ i) {
             printf("|%7.2f%%", pers[i]);
         }
-        printf(" (unit: us)\n");
+        printf(" (unit: us)\t(%10d requests)\n", (int)lat_buf.size());
         printf("%8.2Lf|%8.2Lf", qps, mean_lat);
         for (int i = 0; i < pers_size; ++ i) {
-            int pos = pers[i] / 100 * max(0ll, delta_sz - 1);
+            int pos = pers[i] / 100 * max(0, (int)lat_buf.size() - 1);
             printf("|%8lld", lat_buf[pos]);
         }
-        char file_name[50];
-        sprintf(file_name, "redis_cluster_latency_%s.csv", config.type.c_str());
-        FILE *fp = fopen(file_name, "a");
-        if(fp == NULL) {
-            return 0;
-        }
-        fprintf(fp, "%8.2LF,%8Lf", qps, mean_lat);
-        for(int i = 0; i < pers_size; ++ i) {
-            int pos = pers[i] / 100 * max(0ll, delta_sz - 1);
-            fprintf(fp, ",%8lld", lat_buf[pos]);
-        }
-        fprintf(fp, "\n");
-        fclose(fp);
-
         printf("\n");
+        fprintf(config.fd_rep, "%8.2LF,%8.2Lf", qps, mean_lat);
+        for(int i = 0; i < pers_size; ++ i) {
+            int pos = pers[i] / 100 * max(0, (int)lat_buf.size() - 1);
+            fprintf(config.fd_rep, ",%8lld", lat_buf[pos]);
+            fflush(config.fd_rep);
+        }
+        fprintf(config.fd_rep, "\n");
+        fflush(config.fd_rep);
         config.display_las_time = ustime();
         config.display_las_index = requests_finished + 1;
     }
-}
-
-void show_total_report() {
-    printf("==============%s==============\n", config.type.c_str());
-    char file_name[50];
-    sprintf(file_name, "%s.lat", config.type.c_str());
-    printf("Latency File:\t %s\n", file_name);
-    FILE *lat_fd = fopen(file_name, "w+");
-    sprintf(file_name, "%s.qps", config.type.c_str());
-    printf("QPS File:\t %s\n", file_name);
-    FILE *qps_fd = fopen(file_name, "w+");
-
-    long double mean_lat = 0;
-    for(auto &lat : config.lats) {
-        fprintf(lat_fd, "%lld\n", (long long)lat);
-        mean_lat += lat;
-    }
-    mean_lat = config.requests_finished ? mean_lat / config.requests_finished : 0;
-    long double qps = config.total_tm ? (long double)config.requests_finished / (config.total_tm / 1000000.0) : 0;
-    fprintf(qps_fd, "%.2Lf\n", qps);
-    fclose(lat_fd);
-    fclose(qps_fd);
-    sort(config.lats.begin(), config.lats.end());
-    double pers[] = {0, 50, 80, 95, 99, 99.9, 100};
-    int pers_size = 7; //sizeof(pers) / sizeof(double);
-    printf("%8s%8s", "QPS", "MEAN");
-    for(int i = 0; i < pers_size; ++ i) {
-        printf("%7.2f%%", pers[i]);
-    }
-    printf(" (unit: us)\n");
-    printf("%8.2Lf%8.2Lf", qps, mean_lat);
-    for (int i = 0; i < pers_size; ++ i) {
-        int pos = pers[i] / 100 * max(0, config.requests_finished - 1);
-        printf("%8lld", config.lats[pos]);
-    }
-    printf("\n");
-    fflush(stdout);
+    //config.status = Config::FINISHED;
+    show_total_report();
 }
 
 void benchmark(const string &type) {
+    char file_name[50];
     config.type = type;
     config.requests_finished = 0;
     config.lats.clear();
     pthread_mutex_init(&config.lats_mutex, NULL);
     config.clients_pd.clear();
     config.clients_pd.resize(config.client_num);
+    config.start_tm = 0;
+    config.display_las_index = 0;
+    config.warmup_requests = config.qps; // default
+    config.status = Config::WARMUP;
+
+    sprintf(file_name, "data/%s.lat", config.type.c_str());
+    config.fd_lat = fopen(file_name, "w+");
+    
+    //sprintf(file_name, "data/%s.qps", config.type.c_str());
+    //config.fd_qps = fopen(file_name, "w+");
+    
+    sprintf(file_name, "data/%s_%lld_report.csv", config.type.c_str(), config.interval);
+    config.fd_rep = fopen(file_name, "w+");
+    
+    config.sem_cnt = config.client_num;
     for(int i = 0; i < config.client_num; ++i) {
         pthread_create(&config.clients_pd[i], NULL, processRequests, &i);
     }
     pthread_create(&config.display_pd, NULL, show_report, NULL);
-    config.display_las_time = config.start_tm = ustime();
-    config.display_las_index = 0;
+
     //dist = ExpDist(config.qps * 1e-6, time(NULL), config.start_tm);
     dist = ExpDist(config.qps * 1e-9, time(NULL), nstime());
     for(int i = 0; i < config.client_num; ++i) {
         pthread_join(config.clients_pd[i], NULL);
     }
     pthread_join(config.display_pd, NULL);
-    config.total_tm = ustime() - config.start_tm;
 
     pthread_mutex_destroy(&config.lats_mutex);
-    show_total_report();
+    //show_total_report();
+    
+    fclose(config.fd_rep);
+    //fclose(config.fd_qps);
+    fclose(config.fd_lat);
 }
 
 bool parseOptions(int argc, const char **argv) {
@@ -284,9 +340,15 @@ bool parseOptions(int argc, const char **argv) {
         } else if(!strcmp(argv[i], "-n")) {
             if(lastarg) goto invalid;
             config.requests = atoi(argv[++ i]);
+        } else if(!strcmp(argv[i], "-r")) {
+            if(lastarg) goto invalid;
+            config.total_round = atoi(argv[++ i]);
         } else if(!strcmp(argv[i], "--qps")) {
             if(lastarg) goto invalid;
             config.qps = atoi(argv[++ i]);
+        } else if(!strcmp(argv[i], "--warmup")) {
+            if(lastarg) goto invalid;
+            config.warmup_requests = atoi(argv[++ i]);
         } else if(!strcmp(argv[i], "--interval")) {
             if(lastarg) goto invalid;
             config.interval = atoi(argv[++ i]);
@@ -300,9 +362,9 @@ invalid:
 
 usage:
     printf(
-        "Usage: redis-openloop-bench [-h <host>] [-p <port>] [-c <client>] [-n <requests>]  [-t <test-type>] [-a <auth>] [--qps <qps>] [--interval <interval>]\n\n"
+        "Usage: redis-openloop-bench [-h <host>] [-p <port>] [-c <client>] [-n <requests>]  [-t <test-type>] [-a <auth>] [--qps <qps>] [--warmup <warmup>] [--interval <interval>] [-r <total-rounds>] \n\n"
         "Examples:\n\n"
-        "redis-openloop-bench -h 127.0.0.1 -p 7000 -n 10000000 -c 20 -t set,get --qps 1000 --interval 1\n\n");
+        "redis-openloop-bench -h 127.0.0.1 -p 7000 -n 10000000 -c 50 -t set,get --qps 100000 --warmup 100000 --interval 1 -r 1200\n\n");
     return false;
 }
 
@@ -316,6 +378,8 @@ int main(int argc, const char **argv) {
     config.task.push_back("GET");
     config.interval = 1;
     config.qps = 100;
+    config.round = 0;
+    config.total_round = 10;
     if(parseOptions(argc, argv) == false) exit(-1);
 
     // 添加一个 redis 服务结点，可以多次调用此函数添加多个服务结点，
@@ -324,6 +388,7 @@ int main(int argc, const char **argv) {
     int max_conns = 200;
     string redis_url = config.ip_addr + ":" + to_string(config.port);
     cluster.set(redis_url.c_str(), max_conns);
+    
     for(auto &type : config.task) {
         transform(type.begin(), type.end(), type.begin(), ::toupper);
         benchmark(type);
